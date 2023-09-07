c9cb78f7b5170021e4388c9c4d20b04917170b23
Eltreum <30246110+eltreum0@users.noreply.github.com>
2023-09-07 06:33:10 -0300

    nil check backdrop and check for no backdrop

--- ElvUI_BenikUI/modules/styles/styles.lua
+++ ElvUI_BenikUI/modules/styles/styles.lua
@@ -25,8 +25,15 @@ end
 
 local function StyleScriptErrorsFrame()
 	local frame = _G.ScriptErrorsFrame
-	if not frame.backdrop.style then
-		frame.backdrop:BuiStyle('Outside')
+	if not frame then return end
+	if frame.backdrop then
+		if not frame.backdrop.style then
+			frame.backdrop:BuiStyle('Outside')
+		end
+	else
+		if not frame.style then
+			frame:BuiStyle('Outside')
+		end
 	end
 end
 
