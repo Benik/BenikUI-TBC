03c3d3b44f32061bf807dc2abc910ea38948d29a
E-Tsx <97974579+E-Tsx@users.noreply.github.com>
2026-06-19 15:15:15 +0200

    Update tokens.lua

--- ElvUI_BenikUI/modules/dashboard/tokens.lua
+++ ElvUI_BenikUI/modules/dashboard/tokens.lua
@@ -205,6 +205,7 @@ function mod:UpdateTokens()
 						end
 					end
 
+					self.tokenFrame.Text:SetJustifyH('CENTER')
 					if E.db.benikui.dashboards.textColor == 1 then
 						self.tokenFrame.Text:SetTextColor(classColor.r, classColor.g, classColor.b)
 					else
